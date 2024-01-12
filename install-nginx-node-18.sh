35bfeaac4f1892d9116c1fcea49d3e344065e3a1
Ravuthz <ravuthz@gmail.com>
2024-01-12 10:38:12 +0700

    Update install-nginx-node-18.sh

--- install-nginx-node-18.sh
+++ install-nginx-node-18.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo apt update 
+sudo apt upgrade -y
+
+sudo apt-get install software-properties-common -y
+sudo apt update
+
 echo "Install Git"
 sudo install git -y
 
@@ -6,7 +12,7 @@ echo "Install Nginx"
 sudo install nginx -y
 
 curl -s https://deb.nodesource.com/setup_18.x | sudo bash
-sudo apt install nodejs -y
+sudo apt-get install nodejs -y
 
 node -v
 
